1f933bf9fab52ae5c868e1de104e15c9697c7634
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-03-05 11:43:00 +0100

    change JS code and improved display of photos

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
+// let activeElement = 0;
+// const timeChange = 5000;
+
+// const imgSection = document.querySelector('.image img')
+// const textSection = document.querySelector('.text-change')
+
+// const imgsSection = ['images/s1a.png', 'images/s2a.png', 'images/s3a.png']
+// const textsSection = ['...something interesting :o', '...something happy :D', '...something  amazing :)']
+
+// function changeElements() {
+//     activeElement++;
+//     if (activeElement == imgsSection.length) {
+//         activeElement = 0;
+//     }
+//     imgSection.src = imgsSection[activeElement];
+//     textSection.textContent = textsSection[activeElement];
+// }
+
+// setInterval(changeElements, timeChange)
+
 let activeElement = 0;
 const timeChange = 5000;
 
 const imgSection = document.querySelector('.image img')
 const textSection = document.querySelector('.text-change')
 
-const imgsSection = ['images/s1a.png', 'images/s2a.png', 'images/s3a.png']
-const textsSection = ['...something interesting :o', '...something happy :D', '...something  amazing :)']
+const imagesList = [{
+        img: 'images/s1a.png',
+        text: '...something interesting :o',
+    },
+    {
+        img: 'images/s2a.png',
+        text: '...something happy :D',
+    },
+    {
+        img: 'images/s3a.png',
+        text: '...something  amazing :)',
+    },
+]
 
-function changeElements() {
+const changeElements = () => {
     activeElement++;
-    if (activeElement == imgsSection.length) {
+    if (activeElement === imagesList.length) {
         activeElement = 0;
     }
-    imgSection.src = imgsSection[activeElement];
-    textSection.textContent = textsSection[activeElement];
+    imgSection.src = imagesList[activeElement].img;
+    textSection.textContent = imagesList[activeElement].text;
 }
 
 setInterval(changeElements, timeChange)
